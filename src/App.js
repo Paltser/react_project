8352e13de2aaa1c8694ede89dbb52082a640d110
markos <markos.paltser@voco.ee>
2023-04-05 12:36:36 +0300

    Komponendi sisustamine andmetega

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import './App.css';
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-    <div className="App">
-        <ExpenseItem></ExpenseItem>
-        <ExpenseItem></ExpenseItem>
-    </div>
-  );
+    const expenses = [
+        {
+            date: new Date(2023, 0, 10),
+            title: 'New book',
+            price: 30.99,
+        },
+        {
+            date: new Date(2023, 0, 10),
+            title: 'New jeans',
+            price: 90.99,
+        }
+    ]
+    return (
+        <div className="App">
+            <ExpenseItem expeseData={expenses[0]}></ExpenseItem>
+            <ExpenseItem expeseData={expenses[1]}></ExpenseItem>
+
+        </div>
+    );
 }
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    console.log(props);
+
     return (
         <div className={'expense-item'}>
-            <div>Date</div>
+            <div>{props.expeseData.date.toString()}</div>
             <div className={'expense-item__description'}>
-                <h2>Title</h2>
-                <div className={'expense-item__price'}>Amount</div>
+                <h2>{props.expeseData.title}</h2>
+                <div className={'expense-item__price'}>{props.expeseData.price}</div>
             </div>
         </div>
     );
