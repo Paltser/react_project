e7f934de40054fe72eaed1ccec240c51c9eba1b3
markos <markos.paltser@voco.ee>
2023-04-13 10:02:07 +0300

    Ülesanne 3 tehtud

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,21 @@
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
+
+    props.expenses.map(expense => console.log(expense.date));
+
+
+    const ExpenseFilterHandler = (selectedYear) => {
+        console.log(selectedYear)
+    }
     return (
         <Card className={'expenses'}>
-            <ExpenseItem expenseData={props.expenses[0]} />
-            <ExpenseItem expenseData={props.expenses[1]} />
-            <ExpenseItem expenseData={props.expenses[2]} />
+            <ExpensesFilter onExpenseFilter={ExpenseFilterHandler}/>
+            <ExpenseItem expenseData={props.expenses[0]}/>
+            <ExpenseItem expenseData={props.expenses[1]}/>
         </Card>
     );
 }
